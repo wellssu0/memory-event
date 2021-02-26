46bbba45a4b35eac562c4695d2d05c21034140bb
wellssu <wellssu0@gmail.com>
2021-02-26 14:04:30 +0800

    feat(hooks): 事件模版配置hooks

--- client/src/hooks/useEventTemplateModalConfig.ts
+++ client/src/hooks/useEventTemplateModalConfig.ts
@@ -1,31 +1,58 @@
 import React from 'react';
+import { getTemplateList } from '@/server/index';
+import { EventCycleMode, CalendarType } from '@/constants/index';
+
+type Value = EventCycleMode | CalendarType | string;
 
 type ModeType = 'add' | 'edit';
 
 interface ModelState{
   display: boolean;
-  mode: ModeType
+  mode: ModeType;
+  templateList: EventTemplate[];
 }
+
+export interface EventTemplate{
+  [key:string]: Value;
+}
+
 
 const useEventTemplateModalConfig = ({
   display = false,
   mode = 'add',
+  templateList = []
 }:ModelState) => {
 
   const [displayETModal,setDisplayETModal] = React.useState<ModelState>({
     display: display,
-    mode: mode
+    mode: mode,
+    templateList: templateList
   });
 
-  const handleCloseETModal = () => {
+  /** 拉取模版列表 */
+  const fetchTemplateList = async () => {
+    console.log('拉取模版列表...')
+    const templateListData = await getTemplateList();
+    setDisplayETModal(displayETModal => {
+      return {
+        ...displayETModal,
+        templateList: templateListData
+      }
+    });
+  }
+
+  /** 关闭编辑模版modal */
+  const handleCloseETModal = (reFetch = false) => {
     setDisplayETModal(displayETModal => {
       return {
         ...displayETModal,
         display: false
       }
     })
+    if(reFetch) fetchTemplateList();
   }
 
+  /** 设置编辑模版modal的回显数据 */
   const handleSetETModal = (obj:ModelState) => {
     setDisplayETModal(displayETModal => {
       return {
@@ -35,6 +62,10 @@ const useEventTemplateModalConfig = ({
       }
     })
   }
+
+  React.useEffect(() => {
+    fetchTemplateList();
+  },[])
 
   return {
     displayETModal,
