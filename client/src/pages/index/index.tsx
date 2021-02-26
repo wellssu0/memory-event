5d3d1cb5ff789d090def4cf73245a8edb1923c5c
wellssu <wellssu0@gmail.com>
2021-02-26 14:08:37 +0800

    feat(page): 修改事件页面

--- client/src/pages/index/index.tsx
+++ client/src/pages/index/index.tsx
@@ -5,17 +5,21 @@ import styles from "./index.modules.scss";
 import Calendar from "@/utils/calendar";
 import CustomTabBar, { SelectedPage } from "@/components/CustomTabBar";
 import EventTemplateDrawer from "@/components/EventTemplateDrawer";
-import EventTemplateModal from "@/components/EventTemplateModal";
+import EventTemplateModal, {EventTemplate} from "@/components/EventTemplateModal";
 import useTabBarConfig from "@/hooks/useTabBarConfig";
 import useEventTemplateModalConfig from "@/hooks/useEventTemplateModalConfig";
+import { getEventList } from '@/server/index';
+
 
 const Index = () => {
+  const [eventList, setEventList] = React.useState([]);
   const { displayTabBar, handleToggleShowTabBar } = useTabBarConfig(true);
   const {
     displayETModal,
     handleCloseETModal,
     handleSetETModal
-  } = useEventTemplateModalConfig({display: false, mode: 'add'});
+  } = useEventTemplateModalConfig({display: false, mode: 'add',templateList: []});
+
 
 
 
@@ -23,9 +27,19 @@ const Index = () => {
   const handleOpenTemplateModal = React.useCallback(() => {
     console.log('执行添加操作');
     handleSetETModal({
+      ...displayETModal,
       display: true,
       mode: 'add'
     })
+  },[])
+
+  /** 打开事件Modal */
+  const handleOpenEventModal = React.useCallback((template_id) => {
+    console.log('执行添加操作,template_id',template_id);
+    // handleSetETModal({
+    //   display: true,
+    //   mode: 'add'
+    // })
   },[])
 
   /** 隐藏custom tabBar，打开模版抽屉 */
@@ -42,12 +56,29 @@ const Index = () => {
     });
   };
 
-  useDidShow(() => {
+  /** 获取事件列表 */
+  const fetchEventList = async () => {
+    const result = await getEventList();
+    console.log('fetchEventList', result);
+
+    setEventList(result);
+  }
+
+  useDidShow(async () => {
     handleHideTabBar();
   });
 
+  React.useEffect(() => {
+    fetchEventList();
+  }, [])
+
   return (
     <View className={styles.pageWrap}>
+      <View className={styles.eventListWrap}>
+        {
+          eventList.map(item => (<View key={item._id}>{item.title}</View>))
+        }
+      </View>
 
       <EventTemplateModal
         display={displayETModal.display}
@@ -56,6 +87,8 @@ const Index = () => {
       />
       <EventTemplateDrawer
         display={!displayTabBar}
+        templateList={displayETModal.templateList}
+        onSelectedTemplate={handleOpenEventModal}
         onCloseDrawer={handleToggleShowTabBar}
         onOpenTemplateModal={handleOpenTemplateModal}
       />
