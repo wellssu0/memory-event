b733c2393b53d0d3bd0ecc1a37f8b4e52ba76dc1
wellssu <wellssu0@gmail.com>
2021-02-26 14:07:16 +0800

    feat(component): 修改事件模版抽屉组件

--- client/src/components/EventTemplateDrawer/index.tsx
+++ client/src/components/EventTemplateDrawer/index.tsx
@@ -3,44 +3,52 @@ import { ITouchEvent, View } from '@tarojs/components'
 import BottomDrawer from '@/components/BottomDrawer';
 import classnames from 'classnames';
 import styles from './index.modules.scss';
-
-
+import { EventTemplate } from '@/components/EventTemplateModal';
 interface Props{
   display: boolean;
+  templateList: EventTemplate[];
   onCloseDrawer: () => void;
+  onSelectedTemplate: (id: string) => void;
   onOpenTemplateModal: () => void;
 }
 const ActionTemplateDrawer: FC<Props> = ({
   display = false,
+  templateList = [],
   onCloseDrawer = () => {},
+  onSelectedTemplate = () => {},
   onOpenTemplateModal = () => {},
 }) => {
 
-
+  /** 阻止冒泡 */
   const handleItemWrapClick = (e:ITouchEvent) => {
     e.stopPropagation();
-    console.log('handleItemWrapClick');
   }
-  const handleItemClick = (e:ITouchEvent,id = 0) => {
+
+  /** 选择模版 */
+  const handleItemClick = (e:ITouchEvent,id: string) => {
     e.stopPropagation();
-    console.log(id);
-
+    onSelectedTemplate(id);
   }
 
+  /** 添加模版 */
   const handleOpenAddTemplateModal = (e:ITouchEvent) => {
     e.stopPropagation();
     onOpenTemplateModal();
   }
+
   return(
     <BottomDrawer
       display={display}
       onCloseDrawer={onCloseDrawer}
     >
       <View className={styles.itemWrap} onClick={(e)=>handleItemWrapClick(e)}>
-
-        {/* <View className={styles.temp} onClick={(e)=>handleItemClick(e,item.id)}>fdsfds</View> */}
+        {
+          templateList.map(item => (<View className={styles.temp} onClick={(e)=>handleItemClick(e,item._id)}>{item.title}</View>))
+        }
         <View className={classnames(styles.temp,styles.addBtn)} onClick={(e)=>handleOpenAddTemplateModal(e)}>+</View>
-
+        {
+          templateList.length === 0 && <View className={styles.tips}> 👈 请先新增事件模版哦</View>
+        }
       </View>
 
     </BottomDrawer>
